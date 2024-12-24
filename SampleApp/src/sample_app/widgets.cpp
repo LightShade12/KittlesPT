5b7640285917d9a4e3123f8bcca4ec729caef46f
LightShade12 <subhamswostikpradhan2004@gmail.com>
2024-12-24 12:39:50 +0530

    workaround for inf/nan ema fps estimate

--- SampleApp/src/sample_app/widgets.cpp
+++ SampleApp/src/sample_app/widgets.cpp
@@ -52,13 +52,35 @@ namespace SampleApp
 		last_frame_time_point = current_frame_time_point;
 	}
 
+	static glm::vec3 color;
+
 	void DeveloperWindow::renderUI()
 	{
 		ImGui::Text("Delta ms(last frame): %.3f ms", delta_time_secs.count() * 1000.0f);
-		ImGui::Text("FPS(last frame): %.3f ms", 1000.0f / (delta_time_secs.count() * 1000.0f));
-		average_fps = glm::mix(average_fps, 1000.0f / (delta_time_secs.count() * 1000.0f), 0.01f);
+		float fps = 1000.0f / (delta_time_secs.count() * 1000.0f);
+		ImGui::Text("FPS(last frame): %.3f ms", fps);
+
+		//TODO: weird; idk
+		float avg = glm::mix(average_fps, fps, 0.01f);
+		if (!isnan(avg) && !isinf(avg))
+		{
+			average_fps = avg;
+		}
+
 		ImGui::Text("EMA FPS: %.3f ms", average_fps);
 		ImGui::Text("Runtime secs: %.3f s",
 			std::chrono::duration_cast<std::chrono::duration<float>>(last_frame_time_point.time_since_epoch()).count() - start_time_secs);
+		ImGui::Separator();
+
+		ImGui::SeparatorText("Camera edit");
+		ImGui::SliderFloat("Exposure", &color.r, 0.0, 100.0, "%.3f unitless", ImGuiSliderFlags_Logarithmic);
+		ImGui::SliderAngle("FOV", &color.r, 0.0, 120.0);
+		ImGui::SeparatorText("Material edit");
+		ImGui::SliderInt("Object selection:", (int*)&color.r, 0, 1);
+		ImGui::ColorEdit3("Albedo factor", &color.r);
+		ImGui::SliderFloat("Metallicity", &color.r, 0, 1);
+		ImGui::SliderFloat("Isotropic roughness", &color.r, 0, 1);
+		ImGui::SliderFloat("Transmission", &color.r, 0, 1);
+		ImGui::SliderFloat("IOR", &color.r, 0, 2);
 	}
 }
