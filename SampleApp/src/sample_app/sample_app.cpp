2c598fab43e8ec752c9cf9beac0769a631fad4d3
LightShade12 <subhamswostikpradhan2004@gmail.com>
2025-01-26 10:28:45 +0530

    prep env for triangles

--- SampleApp/src/sample_app/sample_app.cpp
+++ SampleApp/src/sample_app/sample_app.cpp
@@ -97,111 +97,22 @@ namespace SampleApp
 
 		//Image load
 		int width = 0, height = 0, channels = 0;
-
-		unsigned char* img_data = stbi_load("blackpaint.png", &width, &height, &channels, 3);
+		unsigned char* img_data = stbi_load("grid.png", &width, &height, &channels, 3);
 		KittlesPT::TextureSceneEntity texture0(img_data, width, height, 3);
 		stbi_image_free(img_data);
 		img_data = nullptr;
 		scene.addTexture(texture0);
-		{
-			img_data = stbi_load("grid.png", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture1(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture1);
-		}
-		{
-			img_data = stbi_load("colors2.jpg", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture2(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture2);
-		}
-		{
-			img_data = stbi_load("uvgrid.jpeg", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture3(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture3);
-		}
-		{
-			img_data = stbi_load("rusted-steel_roughness.png", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture4(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture4);
-		}
-		{
-			img_data = stbi_load("worn-metal-studs_normal-ogl.png", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture5(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture5);
-		}
-		{
-			img_data = stbi_load("display1.png", &width, &height, &channels, 3);
-			KittlesPT::TextureSceneEntity texture6(img_data, width, height, 3);
-			stbi_image_free(img_data);
-			img_data = nullptr;
-			scene.addTexture(texture6);
-		}
 
-		//smooth
 		scene.addMaterial(KittlesPT::MaterialSceneEntity(
 			0, glm::vec3(1.0, 1.0, 1.0),
-			4, 0.0f, 0.1f,
+			-1, 0.0f, 0.8f,
 			-1, 0.0f,
 			1.45f,
 			-1, glm::vec3(0.0f), 1.0f,
 			-1, 1.0f
 		));
 
-		//floor
-		scene.addMaterial(KittlesPT::MaterialSceneEntity(
-			3, glm::vec3(0.5, 0.5, 0.5),
-			-1, 0.0f, 0.1f,
-			-1, 0.0f,
-			1.45f,
-			-1, glm::vec3(0.0f), 1.0f,
-			-1, 1.0f
-		));
-
-		//metal
-		scene.addMaterial(KittlesPT::MaterialSceneEntity(
-			2, glm::vec3(0.9f, 0.9f, 0.9f),
-			4, 1.0f, 1.0f,
-			-1, 0.0f,
-			1.45f,
-			-1, glm::vec3(0.0f), 1.0f,
-			5, 0.5f
-		));
-
-		//glass
-		scene.addMaterial(KittlesPT::MaterialSceneEntity(
-			1, glm::vec3(1.0f, 1.0f, 1.0f),
-			-1, 0.0f, 0.0f,
-			4, 1.0f,
-			1.45f,
-			-1, glm::vec3(0.0f), 1.0f,
-			-1, 1.0f
-		));
-
-		//60-watt lightbulb emission; 800 lm; 120,000 nits
-		scene.addMaterial(KittlesPT::MaterialSceneEntity(
-			-1, glm::vec3(0.0f, 0.0f, 0.0f),
-			-1, 0.0f, 0.02f,
-			-1, 0.0f,
-			1.45f,
-			//-1, glm::vec3(0.2f, 0.7f, 1.0f), 12.0e4f,
-			6, glm::vec3(1.0f), 12.0e4f,
-			-1, 1.0f
-		));
-
-		scene.addShape(KittlesPT::SphereSceneEntity(0.5f, glm::vec3(0, 0, -3), 0));
-		scene.addShape(KittlesPT::SphereSceneEntity(0.5f, glm::vec3(-1.5, 0, -3), 2));
-		scene.addShape(KittlesPT::SphereSceneEntity(0.5f, glm::vec3(1.5, 0, -3), 3));
-		scene.addShape(KittlesPT::SphereSceneEntity(0.5f, glm::vec3(0, 1.5, -3), 4));//light source
-		scene.addShape(KittlesPT::SphereSceneEntity(100.0f, glm::vec3(0, -100.5, -3), 1));//textured
+		scene.addShape(KittlesPT::SphereSceneEntity(1.0f, glm::vec3(0, 0, -3), 0));
 
 		m_renderer.loadScene(scene);
 	}
